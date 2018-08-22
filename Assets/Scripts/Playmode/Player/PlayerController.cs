b18ee9f608e6a454e81c8be62a1f6e106d9f5d9b
Bitchboi Pikmin <cdtdcdt@gmail.com>
2018-08-22 15:21:49 -0400

    First commit

--- Assets/Scripts/Playmode/Player/PlayerController.cs
+++ Assets/Scripts/Playmode/Player/PlayerController.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] private KeyCode up = KeyCode.W;
+    //[SerializeField] private KeyCode left = KeyCode.A;
+    //[SerializeField] private KeyCode down = KeyCode.S;
+    //[SerializeField] private KeyCode right = KeyCode.D;
+    private TranslateMover translateMover;
+
+    private void Awake()
+    {
+        translateMover = GetComponent<TranslateMover>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+        var position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        translateMover.Move(Vector3.right);
+
+        if (Input.GetKeyDown(up))
+        {
+            translateMover.Move(Vector3.up);
+        }
+
+    }
 }
